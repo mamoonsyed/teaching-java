ee949812444539cc2a9ec6ec1fcf206b69883fcb
transmatter <transmatter@live.com>
2018-07-27 01:10:58 +0500

    Lesson 1 Hello World

---
+++ basics/BasicsDemo.java
@@ -0,0 +1,17 @@
+class BasicsDemo {
+	static void print() {
+		System.out.println("\n\nInside print ...");
+		System.out.println("Hellow, world!!");
+		System.out.println();
+		System.out.println("Hello, World!!");
+		System.out.println("Hello,");
+		System.out.println(" ");
+		System.out.println("world!!");
+	}
+	
+	public static void main(String[] args) {
+		print();
+	}
+
+}
+
---
+++ basics/HelloWorld.java
@@ -0,0 +1,15 @@
+/*  2018-07-24 14:43
+	Simple Java program
+*/
+
+class HelloWorld {
+	public static void main(String args[]){
+	System.out.println(" Hello World!! ");
+	}
+}
+
+class ByeWorld {
+	public static void main(String args[]){
+	System.out.println(" Goodbye. ");
+	}
+}
