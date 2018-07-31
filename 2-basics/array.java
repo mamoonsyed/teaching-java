13d59a57254a6bdea48aa690d34ace1a14c1e42a
Syed Mamoon Ahmed <transmatter@live.com>
2018-08-01 02:36:28 +0500

    Made changes to Array tutorial

--- 2-basics/array.java
+++ 2-basics/array.java
@@ -1,27 +1,49 @@
+import java.util.Arrays;
+
 class array {
     static void method() {
         System.out.println("Inside arrays ...");
-        int[] array1 = new int[7];
-        array1[0] = 9;
-        array1[1] = 8;
-        array1[2] = 7;
-        array1[3] = 6;
-        array1[4] = 5;
-        array1[5] = 4;
-        array1[6] = 3;
-        // array1[7] = 2; // This is out of index
-        System.out.println("This is the length of array1: "+array1.length);
-        System.out.println("This is first element of array1: "+array1[0]);
-        // System.out.println("This is eighth element of array1: "+array1[7]);
+        int[] intArray = new int[7];
+        intArray[0] = 9;
+        intArray[1] = 8;
+        intArray[2] = 7;
+        intArray[3] = 6;
+        intArray[4] = 5;
+        intArray[5] = 4;
+        intArray[6] = 3;
+        // intArray[7] = 2; // This is out of index
+        System.out.println("This is the length of intArray: "+intArray.length);
+        System.out.println("This is first element of intArray: "+intArray[0]);
+        // System.out.println("This is eighth element of intArray: "+intArray[7]);
         // Compiles fine, but throws a runtime error. 
         // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 7 
 
         // Second way of declaring arrays
-        int[] array2 = new int[] {3,4,5,6,7,8,9};
-        System.out.println("This is the length of array2: "+array2.length);
-        System.out.println("This is first element of array2: "+array2[0]);
+        char[] charArray = {'d','e','a','d'};
+        System.out.println("This is the length charArray: "+charArray.length);
+        System.out.println("This is first element of charArray: "+charArray[0]);
+        // HOW TO PRINT WHOLE ARRAY???
+        // good question, add "import java.util.Arrays;"
+        // THEN:
+        System.out.print("This is the complete charArray: ");
+        System.out.println(Arrays.toString(charArray));
+
+        String[] stringArray = {"apple", "banana", "oranges"};
+        System.out.println("This is the stringArray: "+stringArray.length);
+        System.out.println("This is first element of stringArray: "+stringArray[0]);
+        System.out.println("This is the complete stringArray: "+Arrays.toString(stringArray));
         
-    
+        // OUTPUT:
+        // Inside arrays ...
+        // This is the length of intArray: 7
+        // This is first element of intArray: 9
+        // This is the length charArray: 4
+        // This is first element of charArray: d
+        // This is the complete charArray: [d, e, a, d]
+        // This is the stringArray: 3
+        // This is first element of stringArray: apple
+        // This is the complete stringArray: [apple, banana, oranges]
+
     }
     public static void main (String[] args) {
         method();
---
+++ 2-basics/twoDimArrays.java
@@ -0,0 +1,11 @@
+// LETS TALK ABOUT TWO DIMENSIONAL ARRAYS.
+
+class twoDimArrays {
+    static void method() {
+        System.out.println("Inside method ...");
+        int[][] twoD_Array = 
+    }
+    public static void main(String[] args) {
+        method();
+    }
+}
